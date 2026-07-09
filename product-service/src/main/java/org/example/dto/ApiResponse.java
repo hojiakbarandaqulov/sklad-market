3109c1aed1bd3afa061edacec2193e0f914709ac
“Xojiakbar <andaqulovxojiakbar@gmail.com>
2026-07-09 22:09:08 +0500

    mediaBaseurl url bug fix

--- product-service/src/main/java/org/example/dto/ApiResponse.java
+++ product-service/src/main/java/org/example/dto/ApiResponse.java
@@ -2,21 +2,25 @@ package org.example.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.example.enums.AppLanguage;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private Boolean success;
     private T data;
     private String message;
 
+
     private ApiResponse(Boolean success) {
         this.success = success;
     }
 
-    public ApiResponse(T data, Boolean success) {
+    private ApiResponse(T data, Boolean success) {
         this.data = data;
         this.success = success;
     }
@@ -26,6 +30,16 @@ public class ApiResponse<T> {
         this.success = success;
         this.message = message;
     }
+
+    private ApiResponse(String message, Boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public ApiResponse(T data) {
+        this.data = data;
+    }
+
 
     public static <E> ApiResponse<E> successResponse(E data) {
         return new ApiResponse<>(data, Boolean.TRUE);
@@ -38,4 +52,9 @@ public class ApiResponse<T> {
     public static <E> ApiResponse<E> successResponse() {
         return new ApiResponse<>(Boolean.TRUE);
     }
+
+    public static ApiResponse<String> successResponse(String message) {
+        return new ApiResponse<>(message, Boolean.TRUE);
+    }
+
 }
