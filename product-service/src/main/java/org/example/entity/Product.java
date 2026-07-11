7962cd8ad87de6a797b78d37f97058019f669bd8
“Xojiakbar <andaqulovxojiakbar@gmail.com>
2026-07-11 23:15:52 +0500

    mediaBaseurl url bug fix

--- product-service/src/main/java/org/example/entity/Product.java
+++ product-service/src/main/java/org/example/entity/Product.java
@@ -56,11 +56,6 @@ public class Product extends BaseEntity {
     @Column(nullable = false)
     private Currency currency;
 
-    @Column(nullable = false)
-    private Long regionId;
-
-    private Long districtId;
-
     private Long minProduct;
 
     @JdbcTypeCode(SqlTypes.JSON)
